f488b874eefb23604ebc7de04ba44b7969512187
Matheus Vieira <matheus.fvp@outlook.com>
2024-04-18 09:43:21 -0300

    recuperar dados com jdbc

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,10 +3,29 @@ package application;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Connection conn = null;
+        Statement  stmt = null;
+        ResultSet  rs   = null;
+
+        try {
+            conn = DB.getConnection();
+            stmt = conn.createStatement();
+            rs   = stmt.executeQuery("SELECT * FROM department");
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DB.closeStatement(stmt);
+            DB.closeResultSet(rs);
+            DB.closeConnection();
+        }
     }
 }

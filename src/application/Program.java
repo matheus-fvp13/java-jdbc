902f781e8315846ece18cab2ecf6be5d503c7cbd
Matheus Vieira <matheus.fvp@outlook.com>
2024-04-18 10:09:44 -0300

    inserir registro com retorno do id gerado

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,8 @@ public class Program {
                     "INSERT INTO seller "
                             + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
                             + "VALUES "
-                            + "(?, ?, ?, ?, ?)");
+                            + "(?, ?, ?, ?, ?)",
+                        Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, "Luiz Gustavo");
             stmt.setString(2, "lg@gmail.com");
             stmt.setDate(3, new java.sql.Date(sdf.parse("13/08/2002").getTime()));
@@ -25,7 +26,15 @@ public class Program {
             stmt.setInt(5, 4);
 
             int rowsAffected = stmt.executeUpdate();
-            System.out.println("Done! Rows affected: " + rowsAffected);
+            if (rowsAffected > 0) {
+                ResultSet rs = stmt.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done! id = " + id);
+                }
+            }else {
+                System.out.println("No rows affected");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ParseException e) {

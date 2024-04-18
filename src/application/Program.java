086a2d3f48f0256d0182a0f2293f7d4568586065
Matheus Vieira <matheus.fvp@outlook.com>
2024-04-18 10:05:35 -0300

    inserir registro sem retornar informação

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,29 +2,36 @@ package application;
 
 import db.DB;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
-        Connection conn = null;
-        Statement  stmt = null;
-        ResultSet  rs   = null;
-
+        SimpleDateFormat sdf   = new SimpleDateFormat("dd/MM/yyyy");
+        Connection conn        = null;
+        PreparedStatement stmt = null;
         try {
             conn = DB.getConnection();
-            stmt = conn.createStatement();
-            rs   = stmt.executeQuery("SELECT * FROM department");
-            while (rs.next()) {
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-            }
-        }catch(SQLException e) {
+            stmt = conn.prepareStatement(
+                    "INSERT INTO seller "
+                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                            + "VALUES "
+                            + "(?, ?, ?, ?, ?)");
+            stmt.setString(1, "Luiz Gustavo");
+            stmt.setString(2, "lg@gmail.com");
+            stmt.setDate(3, new java.sql.Date(sdf.parse("13/08/2002").getTime()));
+            stmt.setDouble(4, 3000.0);
+            stmt.setInt(5, 4);
+
+            int rowsAffected = stmt.executeUpdate();
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
             e.printStackTrace();
         }finally {
             DB.closeStatement(stmt);
-            DB.closeResultSet(rs);
             DB.closeConnection();
         }
     }
